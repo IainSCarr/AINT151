6485f25c5ae4476f5c2dee5ffbd3398a16715f1f
Iain Carr <33032036+IainSCarr@users.noreply.github.com>
2017-11-20 17:41:07 +0000

    Class Work 5 Complete
    All work completed

--- Class Work/Week 5 - Adv Javascript/Exercise1/main.js
+++ Class Work/Week 5 - Adv Javascript/Exercise1/main.js
@@ -7,13 +7,14 @@ var weaponsArray = [
 
 function OnLoad()
 {
-	
+	for (var i = 0; i < weaponsArray.length; i++) {
+		var weaponTag = "<option value='" + i + "'>" + weaponsArray[i] + "</option>";
+		document.forms[0]["weapons"].innerHTML += weaponTag;
+	}
 }
 
 
 function SelectWeapon()
 {
-	
+	document.getElementById('currentWeapon').innerHTML = weaponsArray[document.forms[0]["weapons"].value];
 }
-
-
--- Class Work/Week 5 - Adv Javascript/Exercise4/main.js
+++ Class Work/Week 5 - Adv Javascript/Exercise4/main.js
@@ -5,5 +5,15 @@ function OnLoad()
 
 function SelectRoom(roomIndex)
 {
-	
+		document.getElementById('roomTitle').innerHTML = roomArray[roomIndex].title;
+		document.getElementById('roomText').innerHTML = roomArray[roomIndex].text;
+
+		document.getElementById('roomChoices').innerHTML = "";
+
+		for (var i = 0; i < roomArray[roomIndex].choices.length; i++) {
+			var index = roomArray[roomIndex].choices[i].index;
+			var button = "<button onclick='SelectRoom(" + index + ")'>" + roomArray[roomIndex].choices[i].text + "</button>"
+			document.getElementById('roomChoices').innerHTML += button;
+		}
+
 }
