aee22e6c0fe645871dccb149f49ed88e25f58029
Iain Carr <33032036+IainSCarr@users.noreply.github.com>
2017-11-23 17:03:11 +0000

    Added initial options
    Initial prototype

---
+++ Class Work/Hypertext Fiction Minigame/data.js
@@ -0,0 +1,74 @@
+var roomArray = [
+	{
+		title:'The Pit',
+		text:'Hiking through the mountains the ground gives way and you fall into a deep chasm. You awake, bruised and bloody, on a cold slab in a dank, cavernous room. Above you a glimmer of light shines down from where you fell, to your east is what looks like a training room while west is a dark, ominous room. As further investigate you notice a small crevice from which red light emanates.',
+		choices:[
+			{
+				text:'Scream towards the light for help.',
+				index:1
+			},
+			{
+				text:'Move towards the training room.',
+				index:2
+			},
+			{
+				text:'Go into the dark room.',
+				index:3
+			},
+			{
+				text:'Squeeze through the crevice.',
+				index:4
+			}
+		]
+	},
+	{
+		title:'The Pit',
+		text:'You let out a scream in the hope that your companions might hear it from above. The sound echoes down the passageways before all falls silent again. After a brief moment a deep rumble accompanied by a rush of wind approaches you; it sends a chill down your spine as you sense a great evil lurking in this strange place.',
+		choices:[
+			{
+				text:'Move towards the training room.',
+				index:2
+			},
+			{
+				text:'Go into the dark room.',
+				index:3
+			},
+			{
+				text:'Squeeze through the crevice.',
+				index:4
+			}
+		]
+	},
+	{
+		title:'Training Room',
+		text:'Creeping forwards down the passageway you approach what looks like a training room scattered with broken sticks and shards of metal. The room has archery targets and mannequins made of straw while the ground is stained red in places. To your east you catch a glint of metal through reinforced doors. South is an archway that leads a large courtyard.',
+		choices:[
+			{
+				text:'Go back to where you fell.',
+				index:5
+			},
+			{
+				text:'Head through the reinforced doors.',
+				index:6
+			},
+			{
+				text:'Approach the courtyard.',
+				index:7
+			}
+		]
+	},
+	{
+		title:'room 4',
+		text:'this is a test',
+		choices:[
+			{
+				text:'Go to room 1',
+				index:0
+			},
+			{
+				text:'Go to room 2',
+				index:1
+			}
+		]
+	}
+]
