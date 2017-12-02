4d855b725aad5ac467e964e28cdb6e00f971c7f7
Iain Carr <33032036+IainSCarr@users.noreply.github.com>
2017-12-02 16:17:21 +0000

    Replaced Links

--- Class Work/Hypertext Fiction Minigame/data.js
+++ Class Work/Hypertext Fiction Minigame/data.js
@@ -2,7 +2,7 @@ var roomArray = [
 	{
 		title:'The Pit',
 		text:'Hiking through the mountains the ground gives way and you fall into a deep chasm. You awake, bruised and bloody, on a cold slab in a dank, cavernous room. Above you a glimmer of light shines down from where you fell, to your east is what looks like a training room while west is a dark, ominous room. As further investigate you notice a small crevice from which red light emanates.',
-		picture:'assets/full_heart.png',
+		picture:'assets/blood.png',
 		choices:[
 			{
 				text:'Scream towards the light for help.',
@@ -25,7 +25,7 @@ var roomArray = [
 	{
 		title:'The Pit',
 		text:'You let out a scream in the hope that your companions might hear it from above. The sound echoes down the passageways before all falls silent again. After a brief moment a deep rumble accompanied by a rush of wind approaches you; it sends a chill down your spine as you sense a great evil lurking in this strange place.',
-		picture:'assets/full_heart.png',
+		picture:'assets/blood.png',
 		choices:[
 			{
 				text:'Move towards the training room.',
@@ -44,7 +44,7 @@ var roomArray = [
 	{
 		title:'Training Room',
 		text:'Creeping forwards down the passageway you approach what looks like a training room scattered with broken sticks and shards of metal. The room has archery targets and mannequins made of straw while the ground is stained red in places. To your east you catch a glint of metal through reinforced doors. South is an archway that leads a large courtyard.',
-		picture:'assets/full_heart.png',
+		picture:'assets/target.png',
 		choices:[
 			{
 				text:'Go back to where you fell.',
@@ -63,7 +63,7 @@ var roomArray = [
 	{
 		title:'room 4',
 		text:'this is a test',
-		picture:'assets/full_heart.png',
+		picture:'assets/skull.png',
 		choices:[
 			{
 				text:'Go to room 1',
