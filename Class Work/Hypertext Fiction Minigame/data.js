4fef2dd1ac09871136de3bbce807c91f52dd2544
Iain Carr <33032036+IainSCarr@users.noreply.github.com>
2017-12-04 16:41:16 +0000

    Update
    Audio option added and functions added for modularity.

--- Class Work/Hypertext Fiction Minigame/data.js
+++ Class Work/Hypertext Fiction Minigame/data.js
@@ -3,6 +3,7 @@ var roomArray = [
 		title:'The Pit',
 		text:'Hiking through the mountains the ground gives way and you fall into a deep chasm. You awake, bruised and bloody, on a cold slab in a dank, cavernous room. Above you a glimmer of light shines down from where you fell, to your east is what looks like a training room while west is a dark, ominous room. As further investigate you notice a small crevice from which red light emanates.',
 		picture:'assets/blood.png',
+		audio:'',
 		choices:[
 			{
 				text:'Scream towards the light for help.',
@@ -26,6 +27,7 @@ var roomArray = [
 		title:'The Pit',
 		text:'You let out a scream in the hope that your companions might hear it from above. The sound echoes down the passageways before all falls silent again. After a brief moment a deep rumble accompanied by a rush of wind approaches you; it sends a chill down your spine as you sense a great evil lurking in this strange place.',
 		picture:'assets/blood.png',
+		audio:'assets/manscream.wav',
 		choices:[
 			{
 				text:'Move towards the training room.',
@@ -45,6 +47,7 @@ var roomArray = [
 		title:'Training Room',
 		text:'Creeping forwards down the passageway you approach what looks like a training room scattered with broken sticks and shards of metal. The room has archery targets and mannequins made of straw while the ground is stained red in places. To your east you catch a glint of metal through reinforced doors. South is an archway that leads a large courtyard.',
 		picture:'assets/target.png',
+		audio:'assets/civwin.wav',
 		choices:[
 			{
 				text:'Go back to where you fell.',
@@ -64,6 +67,7 @@ var roomArray = [
 		title:'room 4',
 		text:'this is a test',
 		picture:'assets/skull.png',
+		audio:'',
 		choices:[
 			{
 				text:'Go to room 1',
--- Class Work/Hypertext Fiction Minigame/main.js
+++ Class Work/Hypertext Fiction Minigame/main.js
@@ -9,17 +9,38 @@ function SelectRoom(roomIndex)
 		document.getElementById('roomText').innerHTML = roomArray[roomIndex].text;
 
 		document.getElementById('roomChoices').innerHTML = "";
-		document.getElementById('image').innerHTML = ""
-;
-		for (var i = 0; i < roomArray[roomIndex].choices.length; i++) {
-			var index = roomArray[roomIndex].choices[i].index;
-			var button = "<button onclick='SelectRoom(" + index + ")'>" + roomArray[roomIndex].choices[i].text + "</button></br>"
-			document.getElementById('roomChoices').innerHTML += button;
-		}
+		document.getElementById('image').innerHTML = "";
 
 
-		var img = roomArray[roomIndex].picture
-		var picture = "<img src='" + img + "'>" ;
-		document.getElementById('image').innerHTML += picture;
+		AddButtons(roomIndex);
 
+		AddPicture(roomIndex);
+
+		AddAudio(roomIndex);
+
+}
+
+function AddButtons(roomIndex)
+{
+	for (var i = 0; i < roomArray[roomIndex].choices.length; i++) {
+		var index = roomArray[roomIndex].choices[i].index;
+		var button = "<button onclick='SelectRoom(" + index + ")'>" + roomArray[roomIndex].choices[i].text + "</button></br>"
+		document.getElementById('roomChoices').innerHTML += button;
+	}
+}
+
+
+function AddPicture(roomIndex)
+{
+	var img = roomArray[roomIndex].picture;
+	var picture = "<img src='" + img + "'>" ;
+	document.getElementById('image').innerHTML += picture;
+}
+
+
+function AddAudio(roomIndex)
+{
+	var aud = roomArray[roomIndex].audio
+	var audio = "<audio autoplay><source src='" + aud + "' " + "type='audio/wav'>Your browser does not support the audio element.</audio>"
+	document.getElementById('audio').innerHTML = audio;
 }
